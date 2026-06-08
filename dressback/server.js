41728e24ea4ae8129c0d88c37a74ee8ce96fe41c
Jisna <helenjisna@gmail.com>
2026-06-08 09:22:36 +0000

    test

--- dressback/server.js
+++ dressback/server.js
@@ -1,15 +1,20 @@
+require("dotenv").config();
+
 const app = require("./src/app");
 
-const PORT = 5000;
+const uploadRoutes =
+  require("./src/routes/upload.routes");
+
+app.use(
+  "/upload",
+  uploadRoutes
+);
+
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
-
-const uploadRoutes = require("./src/routes/upload.routes");
-  app.use("/upload",uploadRoutes
-);
-require("dotenv").config();
-console.log(
-  process.env.AWS_BUCKET_NAME
-);
